72fd21f167cb67b9e7b4fc9d740bde5ff89b2b4f
adrier.santos <adrier.santos@newtail.com.br>
2023-05-01 14:41:45 -0300

    armazenando dados no apacha cassandra

--- scr/connect_bd.py
+++ scr/connect_bd.py
@@ -1,12 +1,21 @@
 from cassandra.cluster import Cluster
-
+import main
+import uuid
 # Cria um objeto de cluster Cassandra e se conecta
 cluster = Cluster(['localhost'])
-session = cluster.connect()
+session = cluster.connect('dados_climaticos')
 
+id = uuid.uuid1()
 # Consulta os dados
-# result_set = session.execute("SELECT * FROM curso_cassandra.alunos;")
-# keyspaces = session.execute("DESCRIBE KEYSPACES;")
+query = f"INSERT INTO dados_clima (id, id_cidade, nome, estado, pais, temperatura, humidade, condicao, sensacao_termica, dia) " \
+        f"VALUES ({id}, {main.data_json['id_cidade']}, '{main.data_json['nome']}', '{main.data_json['estado']}', " \
+        f"'{main.data_json['pais']}', {main.data_json['temperatura']}, {main.data_json['humidade']}, " \
+        f"'{main.data_json['condicao']}', {main.data_json['sensacao_termica']}, '{main.data_json['dia']}');"
+session.execute(query)
 
-# Fecha a conexão
-# cluster.shutdown()
+# query = 'SELECT * FROM dados_clima'
+# dados = (session.execute(query))
+# for i in dados:
+#         print(i)
+#
+
--- scr/functions/advisor_API.py
+++ scr/functions/advisor_API.py
@@ -19,17 +19,3 @@ def meteorological_data(city_id, paramts):
     else:
         return Exception(requests.status_codes)
 
-def format_data(list_city):
-    for i in range(len(list_city)):
-        data_formating = {
-            "id_cidade": list_city[i]['id'],
-            "nome": list_city[i]['name'],
-            "estado": list_city[i]['state'],
-            "pais": list_city[i]['country'],
-            "temperatura": list_city[i]['data']['temperature'],
-            "humidade": list_city[i]['data']['humidity'],
-            "condicao": list_city[i]['data']['condition'],
-            "sesacao_termica": list_city[i]['data']['sensation'],
-            "dia": list_city[i]['data']['date']
-        }
-        return data_formating
