127b89234725c69e9583ae84275df701c71851a0
adrier <adrierj@gmail.com>
2023-05-20 00:05:09 -0300

    adicionando novos dados vindo da API

--- scr/send_email.py
+++ scr/send_email.py
@@ -1,5 +1,5 @@
 import yagmail
-import connect_bd as banco
+import main
 from dotenv import load_dotenv
 import os
 
@@ -11,19 +11,22 @@ sender_email = 'dadosclimaticos.uepb@gmail.com'
 sender_password = PASSWORD
 
 # Configuração do destinatário e mensagem
-emails = ['adrier.santos']
+emails = ['ludmila.lima']
 # recipient = 'adrier.santos@aluno.uepb.edu.br'
-subject = 'Dados climáticos - Patos - PB'
-dados = []
-dados.append(banco.linhas_ordenadas[-1])
-body = f'{dados}'
-print(body)
+
+
+
 # Cria o objeto yagmail e envia o e-mail
 
 for i in range (len(emails)):
+    dados = main.chamar_API()
+    body = f'{dados}'
+    subject = f'Dados climáticos - Patos - PB'
     dominio = '@aluno.uepb.edu.br'
     recipient = emails[i]+dominio
     yag = yagmail.SMTP(sender_email, sender_password)
     yag.send(to=recipient, subject=subject, contents=body)
     print(f"Email enviado para {recipient}")
+    print(body)
+
 
