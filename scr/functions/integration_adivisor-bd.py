df87e418b7cac19780096b7c0ae3a8f80f3588dd
adrier <adrierj@gmail.com>
2023-05-20 18:53:03 -0300

    implementação do html do e-mail

--- scr/functions/integration_adivisor-bd.py
+++ scr/functions/integration_adivisor-bd.py
@@ -1,10 +1,6 @@
 from scr.connect_bd import *
 from scr.main import *
 
-#
-# cursor.execute('select * from dados_clima_diarios')
-# result = cursor.fetchall()
-
 def insert_data(current_weather_data):
     comando_sql = """
         INSERT INTO dados_clima_diarios (id_cidade, nome, estado, pais, temperatura, direcao_vento, velocidade_vento, humidade, condicao, pressao, sensacao_termica, data_atual)
@@ -17,8 +13,16 @@ def insert_data(current_weather_data):
     except (Exception, psycopg2.Error) as error:
         print("Erro ao inserir dados:", error)
 
+def search_data():
+    pass
+
 
 current_weather_data = chamar_API()
 
-insert_data(current_weather_data)
+# insert_data(current_weather_data)
 
+cursor.execute('select * from dados_clima_diarios')
+result = cursor.fetchall()
+
+for row in result:
+    print(row)
--- scr/functions/send_email.py
+++ scr/functions/send_email.py
@@ -2,6 +2,7 @@ import yagmail
 from scr.main import *
 from dotenv import load_dotenv
 import os
+from jinja2 import Environment, FileSystemLoader
 
 
 load_dotenv()
@@ -10,23 +11,27 @@ PASSWORD = os.getenv('PASSWORD')
 sender_email = 'dadosclimaticos.uepb@gmail.com'
 sender_password = PASSWORD
 
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('template.html')
+
+
 # Configuração do destinatário e mensagem
-emails = ['adrier.santos']
+emails = ['adrier.santos', 'ronildo.junior', 'gabriel.lira','elder.andrade']
 # recipient = 'adrier.santos@aluno.uepb.edu.br'
-
-
 
 # Cria o objeto yagmail e envia o e-mail
 
 for i in range (len(emails)):
     dados = chamar_API()
-    body = f'{dados}'
+    nome = emails[i]
+    #Funcao para adicionar varíaveis dinâmicas no html
+    html_output = template.render(nome=nome, dados=dados)
+    body = html_output
     subject = f'Dados climáticos - Patos - PB'
     dominio = '@aluno.uepb.edu.br'
     recipient = emails[i]+dominio
     yag = yagmail.SMTP(sender_email, sender_password)
     yag.send(to=recipient, subject=subject, contents=body)
     print(f"Email enviado para {recipient}")
-    print(body)
 
 
