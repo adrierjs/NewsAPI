2101f643511a6b28754ec8025d85d1152d3d5d1a
Ronildo Lima <ronildolima996@gmail.com>
2023-05-20 20:07:02 -0300

    Alterando localização do template

--- scr/functions/send_email.py
+++ scr/functions/send_email.py
@@ -4,34 +4,33 @@ from dotenv import load_dotenv
 import os
 from jinja2 import Environment, FileSystemLoader
 
-
 load_dotenv()
 PASSWORD = os.getenv('PASSWORD')
 # Configuração da conta de e-mail
 sender_email = 'dadosclimaticos.uepb@gmail.com'
 sender_password = PASSWORD
 
-env = Environment(loader=FileSystemLoader('.'))
+# Obtém o diretório do script atual
+base_dir = os.path.dirname(os.path.abspath(__file__))
+# Define o diretório do template
+template_dir = os.path.join(base_dir, 'scr', '..')
+env = Environment(loader=FileSystemLoader(template_dir))
 template = env.get_template('template.html')
-
 
 # Configuração do destinatário e mensagem
 emails = ['adrier.santos', 'ronildo.junior', 'gabriel.lira','elder.andrade']
-# recipient = 'adrier.santos@aluno.uepb.edu.br'
 
 # Cria o objeto yagmail e envia o e-mail
 
-for i in range (len(emails)):
+for email in emails:
     dados = chamar_API()
-    nome = emails[i]
-    #Funcao para adicionar varíaveis dinâmicas no html
+    nome = email
+    # Função para adicionar variáveis dinâmicas no HTML
     html_output = template.render(nome=nome, dados=dados)
     body = html_output
-    subject = f'Dados climáticos - Patos - PB'
+    subject = 'Dados climáticos - Patos - PB'
     dominio = '@aluno.uepb.edu.br'
-    recipient = emails[i]+dominio
+    recipient = email + dominio
     yag = yagmail.SMTP(sender_email, sender_password)
     yag.send(to=recipient, subject=subject, contents=body)
     print(f"Email enviado para {recipient}")
-
-
