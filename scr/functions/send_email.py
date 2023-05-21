612eb119818ee594d516acd53c8c423118d6e3ba
adrier <adrierj@gmail.com>
2023-05-21 16:15:23 -0300

    removendo biblioteca problemática

--- scr/functions/send_email.py
+++ scr/functions/send_email.py
@@ -2,7 +2,7 @@ import yagmail
 from scr.main import *
 from dotenv import load_dotenv
 import os
-from jinja2 import Environment, FileSystemLoader
+
 
 load_dotenv()
 PASSWORD = os.getenv('PASSWORD')
@@ -10,24 +10,15 @@ PASSWORD = os.getenv('PASSWORD')
 sender_email = 'dadosclimaticos.uepb@gmail.com'
 sender_password = PASSWORD
 
-# Obtém o diretório do script atual
-base_dir = os.path.dirname(os.path.abspath(__file__))
-# Define o diretório do template
-template_dir = os.path.join(base_dir, 'scr', '..')
-env = Environment(loader=FileSystemLoader(template_dir))
-template = env.get_template('template.html')
-
 # Configuração do destinatário e mensagem
 emails = ['adrier.santos', 'ronildo.junior', 'gabriel.lira','elder.andrade']
 
 # Cria o objeto yagmail e envia o e-mail
 
 for email in emails:
-    dados = chamar_API()
+    body = chamar_API()
     nome = email
     # Função para adicionar variáveis dinâmicas no HTML
-    html_output = template.render(nome=nome, dados=dados)
-    body = html_output
     subject = 'Dados climáticos - Patos - PB'
     dominio = '@aluno.uepb.edu.br'
     recipient = email + dominio
