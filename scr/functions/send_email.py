034a0b26a219b60533a0f0ac7670bee3dd96c494
adrier <adrierj@gmail.com>
2023-05-23 01:21:13 -0300

    refatoração e tratamento de dados

--- scr/functions/send_email.py
+++ scr/functions/send_email.py
@@ -1,37 +1,27 @@
 import yagmail
-from scr.main import *
 from dotenv import load_dotenv
+from NewsAPI.dataFormatingNews import listNews
 import os
-from jinja2 import Environment, FileSystemLoader
+
+def sendEmailNews(listNews, emails,sender_email, sender_password):
+    subject = 'Newlatter Computing - UEPB'
+    for i in range(len(emails)):
+        dominio = '@aluno.uepb.edu.br'
+        recipient = emails[i] + dominio
+        yag = yagmail.SMTP(sender_email, sender_password)
+        yag.send(to=recipient, subject=subject, contents=listNews)
+        print(f"Email enviado para {recipient}")
 
 
 load_dotenv()
 PASSWORD = os.getenv('PASSWORD')
-# Configuração da conta de e-mail
-# sender_email = 'dadosclimaticos.uepb@gmail.com'
-# sender_password = PASSWORD
-
-# env = Environment(loader=FileSystemLoader('.'))
-# template = env.get_template('template.html')
+sender_email = 'dadosclimaticos.uepb@gmail.com'
+sender_password = PASSWORD
+emails = ['adrier.santos']
+sendEmailNews(listNews, emails,sender_email, sender_password)
 
 
-# Configuração do destinatário e mensagem
-emails = ['adrier.santos', 'ronildo.junior', 'gabriel.lira','elder.andrade']
-# recipient = 'adrier.santos@aluno.uepb.edu.br'
 
-# Cria o objeto yagmail e envia o e-mail
 
-for i in range (len(emails)):
-    dados = chamar_API()
-    nome = emails[i]
-    #Funcao para adicionar varíaveis dinâmicas no html
-    html_output = template.render(nome=nome, dados=dados)
-    body = dados
-    subject = f'Dados climáticos - Patos - PB'
-    dominio = '@aluno.uepb.edu.br'
-    recipient = emails[i]+dominio
-    yag = yagmail.SMTP(sender_email, sender_password)
-    yag.send(to=recipient, subject=subject, contents=body)
-    print(f"Email enviado para {recipient}")
 
 
