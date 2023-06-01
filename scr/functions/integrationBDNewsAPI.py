f47cce0e0d6960fda24bcac82f464e3609b8dfd9
adrier <adrierj@gmail.com>
2023-06-01 19:50:17 -0300

    recebendo e-mails do banco de dados

--- scr/functions/integrationBDNewsAPI.py
+++ scr/functions/integrationBDNewsAPI.py
@@ -3,5 +3,6 @@ from scr.connect_bd import *
 cursor.execute('select * from tabela_usuarios')
 result = cursor.fetchall()
 
+list_emails = []
 for row in result:
-    print(row)
+    list_emails.append(row[2])
--- scr/functions/send_email.py
+++ scr/functions/send_email.py
@@ -1,13 +1,13 @@
 import yagmail
 from dotenv import load_dotenv
 from NewsAPI.dataFormatingNews import listNews
+from integrationBDNewsAPI import list_emails
 import os
 
 def sendEmailNews(listNews, emails,sender_email, sender_password):
     subject = 'Newlatter Computing - UEPB'
     for i in range(len(emails)):
-        dominio = '@aluno.uepb.edu.br'
-        recipient = emails[i] + dominio
+        recipient = emails[i]
         yag = yagmail.SMTP(sender_email, sender_password)
         yag.send(to=recipient, subject=subject, contents=listNews)
         print(f"Email enviado para {recipient}")
@@ -17,8 +17,7 @@ load_dotenv()
 PASSWORD = os.getenv('PASSWORD')
 sender_email = 'dadosclimaticos.uepb@gmail.com'
 sender_password = PASSWORD
-emails = ['adrier.santos']
-sendEmailNews(listNews, emails,sender_email, sender_password)
+sendEmailNews(listNews, list_emails,sender_email, sender_password)
 
 
 
